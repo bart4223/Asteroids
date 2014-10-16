d38a80555fe0aca3f2e72f9035585b1f2e7f2d60
Nils <nilsgrimmer@gmx.de>
2014-10-17 00:09:45 +0200

    Title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Asteroids");
+        primaryStage.setScene(new Scene(root, 300, 300));
         primaryStage.show();
     }
 
