61263d89eff8121c1e3161ec0c321cd7f674e0e3
Nils <nilsgrimmer@gmx.de>
2014-12-22 14:10:11 +0100

    Use Uniplay engine

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,35 @@
 package sample;
 
+import Uniplay.NGGameEngine;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Main extends Application {
 
+    protected NGGameEngine FGameEngine;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Asteroids");
-        primaryStage.setScene(new Scene(root, 300, 300));
-        primaryStage.show();
+        FGameEngine = new NGGameEngine(null);
+        Properties configuration = new Properties();
+        InputStream is = new FileInputStream("resources/config.as");
+        configuration.load(is);
+        FGameEngine.setConfigurationFilename(configuration.getProperty("UniplayConfigurationFilename"));
+        FGameEngine.Initialize();
+        FGameEngine.Startup();
     }
 
+    @Override
+    public void stop() throws Exception {
+        FGameEngine.Shutdown();
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
